fefecf47de725c42459953ba3c072af48db1b425
Marc-Velay <marc.velay@gmail.com>
2023-01-28 11:27:25 +0100

    added ppo agent configs

--- agents/agent_hp_setup.py
+++ agents/agent_hp_setup.py
@@ -40,6 +40,39 @@ def conf_ddpg(cfg: DictConfig):
     
     return hyperparams
 
+def conf_ppo(cfg):
+    activation_fn = {"tanh": nn.Tanh, "relu": nn.ReLU, "elu": nn.ELU, "leaky_relu": nn.LeakyReLU}[cfg.agent.activation]
+
+    net_arch = {
+        "s": [32, 16],
+        "m": [64, 32],
+        "l": [64, 32, 16],
+    }[cfg.agent.net_arch]
+
+    hyperparams = {
+        "gamma": cfg.agent.gamma,
+        "learning_rate": cfg.agent.learning_rate,
+        "batch_size": cfg.agent.batch_size,
+        "n_steps": cfg.agent.n_steps,
+        "n_epochs": cfg.agent.n_epochs,
+        "ent_coef": cfg.agent.ent_coef,
+        "clip_range": cfg.agent.clip_range,
+        "gae_lambda": cfg.agent.gae_lambda,
+        "max_grad_norm": cfg.agent.max_grad_norm,
+        "vf_coef": cfg.agent.vf_coef,
+        "policy_kwargs": dict(
+            net_arch=net_arch,
+            activation_fn=activation_fn
+        ),
+    }
+
+    if hyperparams["batch_size"] > hyperparams["n_steps"]:
+        hyperparams["batch_size"] = hyperparams["n_steps"]
+    
+    return hyperparams
+
+
 AGENTS_HP = {
     "ddpg": conf_ddpg,
+    "ppo": conf_ppo,
 }
--- agents/agent_register.py
+++ agents/agent_register.py
@@ -2,10 +2,6 @@ from stable_baselines3 import A2C, DDPG, DQN, PPO, SAC, TD3
 
 
 AGENTS = {
-    "a2c": A2C,
     "ddpg": DDPG,
-    "dqn": DQN,
     "ppo": PPO,
-    "sac": SAC,
-    "td3": TD3,
 }
